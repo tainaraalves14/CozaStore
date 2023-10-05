705675b9a4c3bb95a6c60a2824d160336e553e4a
Tainara Alves <tainara.nascimento16@etec.sp.gov.br>
2023-10-05 10:16:28 -0300

    Iniciação Atividade

---
+++ cozastore/CozaStore/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using CozaStore.Models;
+
+namespace CozaStore.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Blog()
+    {
+        return View();
+    }
+
+    public IActionResult Contatos()
+    {
+        return View();
+    }
+
+    public IActionResult Loja()
+    {
+        return View();
+    }
+
+    public IActionResult Produtos()
+    {
+        return View();
+    }
+    public IActionResult Termos()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ cozastore/CozaStore/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace CozaStore.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
