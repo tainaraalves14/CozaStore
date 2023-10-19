2bf02276bee4edb9b82c8ef71c2be8f41d64a5f3
Tainara Alves <tainara.nascimento16@etec.sp.gov.br>
2023-10-19 10:06:41 -0300

    correção erros

--- cozastore/CozaStore/Models/Produto.cs
+++ cozastore/CozaStore/Models/Produto.cs
@@ -3,35 +3,51 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace CozaStore.Models;
 
-
 [Table("Produto")]
+
 public class Produto
 {
     [Key]
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-    public int Id {get; set;}
+    public int Id { get; set; }
 
-    [ Required (ErrorMessage = "Informe o Nome")]
-    [StringLength(60, ErrorMessage =  "O Nome deve possuir no máximo 30 caractéries")]
-    public string Nome {get; set;}
+    [Required(ErrorMessage = "Informe o Nome")]
+    [StringLength(60, ErrorMessage = "O nome deve possuir no máximo 60 caracteres")]
+    public string Nome { get; set; }
 
     [Display(Name = "Descrição Resumida")]
-    [ Required (ErrorMessage = "Informe o Nome")]
-    [StringLength(60, ErrorMessage =  "A descrição deve possuir no máximo 300 carctéries")]
-    public string DescricaoResumida {get; set;}
-
+    [StringLength(300, ErrorMessage = "A descrição resumida deve possuir no máximo 30 caracteres")]
+    public string DescricaoResumida { get; set; }
 
     [Display(Name = "Descrição Completa")]
     [StringLength(8000)]
-    public string DecricaoCompleta {get; set;}
+    public string DescricaoCompleta { get; set; }
 
-    [StringLength(10, ErrorMessage = " O SKU deve possur no máximo 10 carctéries")]
-    public string SDU { get; set; }
+    [StringLength(10, ErrorMessage = "O SKU deve posssuir no máximo 10 caracteres")]
+    public string SKU { get; set; }
+
+    [Display(Name = "Preço")]
+    [Column(TypeName = "decimal(8,2)")]
+    [Required(ErrorMessage = "Informe o preço de venda")]
+    public string Preco { get; set; }
 
     [Display(Name = "Preço com Desconto")]
-    [Column(TypeName ="decimal(8,2)")]
-    [Required(ErrorMessage = "Informe o Preço de Venda")]
-    public decimal PrecoDesconto { get; set; }
+    [Column(TypeName = "decimal(8,2)")]
+    [Required(ErrorMessage = "Informe o preço com desconto")]
+    public string PrecoDesconto { get; set; }
 
+    [Display(Name = "Produto em Destaque?")]
+    public bool Destaque { get; set; }
+
+    [Column(TypeName = "decimal(6,3)")]
+    public decimal Peso { get; set; } = 0;
+
+    [StringLength(30, ErrorMessage = "O material deve possuir no máximo 30 caracteres")]
+    public string Material { get; set;}
+
+    [Display(Name = "Dimensões")]
+    [StringLength(30, ErrorMessage = "As dimensões deve possuir no máximo 30 caracteres")]
+    public string Dimensao { get; set;}
+
+    
 }
-
